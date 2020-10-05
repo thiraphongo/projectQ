5cb52c7a03b22123bfc43120f255e0dece937094
thiraphongo <thiraphongo61@nu.ac.th>
2020-10-05 22:51:45 +0700

    folder perpared

---
+++ ProJectQ/app/src/main/java/com/example/projectq/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.projectq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.content.Context;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageButton next = (ImageButton)findViewById(R.id.img8);
+        ImageButton next2 = (ImageButton)findViewById(R.id.img9);
+        ImageButton next3 = (ImageButton)findViewById(R.id.img10);
+        ImageButton next4 = (ImageButton)findViewById(R.id.img17);
+        ImageButton next5 = (ImageButton)findViewById(R.id.img20);
+        ImageButton next6 = (ImageButton)findViewById(R.id.img29);
+        ImageButton next7 = (ImageButton)findViewById(R.id.img31);
+        ImageButton next8 = (ImageButton)findViewById(R.id.img34);
+        ImageButton next9 = (ImageButton)findViewById(R.id.img36);
+        ImageButton next10 = (ImageButton)findViewById(R.id.img38);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this , SecondActivity.class);
+                startActivity(i);
+            }
+        });
+
+        next2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this , SecondActivity2.class);
+                startActivity(i);
+            }
+        });
+
+        next3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this , SecondActivity3.class);
+                startActivity(i);
+            }
+        });
+
+        next4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this , SecondActivity4.class);
+                startActivity(i);
+            }
+        });
+
+        next5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this , SecondActivity5.class);
+                startActivity(i);
+            }
+        });
+
+        next6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this , SecondActivity6.class);
+                startActivity(i);
+            }
+        });
+
+        next7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this , SecondActivity7.class);
+                startActivity(i);
+            }
+        });
+
+        next8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this , SecondActivity8.class);
+                startActivity(i);
+            }
+        });
+
+        next9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this , SecondActivity9.class);
+                startActivity(i);
+            }
+        });
+
+        next10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this , SecondActivity10.class);
+                startActivity(i);
+            }
+        });
+    }
+}
+
+
+
+
